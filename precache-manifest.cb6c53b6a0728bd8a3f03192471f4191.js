9ac101001494324cca1920fb0df3c1190d976806
System Administrator <b316009@iiit-bh.ac.in>
2019-07-17 04:44:14 +0530

    Updates

---
+++ precache-manifest.cb6c53b6a0728bd8a3f03192471f4191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b2dd249ef7818f4d554e6d82896023",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d612125eec5518e5bea7",
+    "url": "/monsters-rolodex/static/css/main.fa262cfe.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d612125eec5518e5bea7",
+    "url": "/monsters-rolodex/static/js/main.210bff61.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
